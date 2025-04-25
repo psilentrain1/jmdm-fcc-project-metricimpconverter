6129e3d2fd965532eb6d2baaa8d107402d3f380f
James Drake <github@jamesmdrakemedia.com>
2025-04-25 03:44:23 +0000

    feat: added first functional test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,39 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
+    this.timeout(5000)
 
+    test('Convert a valid input', function(done){
+        chai
+        .request(server)
+        .keepOpen()
+        .get('/api/convert')
+        .query({input: "3.2mi"})
+        .end(function(err, res){
+            assert.equal(res.status, 200);
+            assert.equal(res.body.initNum, '10');
+            assert.equal(res.body.initUnit, 'L');
+            assert.equal(res.body.returnNum, 2.64172);
+            assert.equal(res.body.returnUnit, 'gal');
+            assert.equal(res.body.string, "10 liters converts to 2.64172 gallons");
+            done();
+        });
+    });
+
+    test('Convert an invalid input', function(done){
+        chai
+        .request(server)
+        .keepOpen()
+        .get('/api/convert')
+        .query({input: "32g"})
+        .end(function(err, res){
+            
+        })
+    })
+
+    test('Convert an invalid number', function(done){})
+
+    test('Convert an invalid number and unit', function(done){})
+
+    test('Convert with no number', function(done){})
 });
